4a7d3d58eaf7cfe161db88d13c829df4a84cc1d1
Adam Slack <adamslack@outlook.com>
2021-03-02 23:12:02 +0000

    device media queries for different layout sizes

--- moistsquid-frontend/src/components/BinDayList/BinDayList.jsx
+++ moistsquid-frontend/src/components/BinDayList/BinDayList.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { BinDayListItem } from '../BinDayListItem'
+import './BinDayList.css'
 
 export const BinDayList = (props) => {
   const { binDays } = props
 
   return (
-    <ul>
+    <ul className="binDayList">
       {
         binDays.map((binDay) => (
           <BinDayListItem key={binDay.binColour} {...binDay} />
