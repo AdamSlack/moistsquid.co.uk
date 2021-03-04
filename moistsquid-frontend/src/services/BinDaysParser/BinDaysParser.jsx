12847d29db247dc9d7b6c0436e191145ca678d7c
Adam Slack <adamslack@outlook.com>
2021-03-04 00:16:09 +0000

    getting data from s3 not from a lambda

--- moistsquid-frontend/src/services/BinDaysParser/BinDaysParser.jsx
+++ moistsquid-frontend/src/services/BinDaysParser/BinDaysParser.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const fetchBinDays = async () => {
-  return (await axios.get('https://u121ovudsh.execute-api.eu-west-2.amazonaws.com/test')).data;
+  return (await axios.get('http://moistsquid.co.uk/binDays/data.json')).data;
 }
 
