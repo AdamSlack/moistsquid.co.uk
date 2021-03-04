1ff5c888779130e56e74e324a3915b5068f0d48c
Adam Slack <adamslack@outlook.com>
2021-03-04 00:41:34 +0000

    http to https

--- moistsquid-frontend/src/services/BinDaysParser/BinDaysParser.jsx
+++ moistsquid-frontend/src/services/BinDaysParser/BinDaysParser.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchBinDays = async () => {
   return (await axios.get(
-    'http://moistsquid.co.uk/binDays/data.json',
+    'https://moistsquid.co.uk/binDays/data.json',
     {
       headers : { 
         'Content-Type': 'application/json',
